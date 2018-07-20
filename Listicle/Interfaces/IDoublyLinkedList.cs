19b213070b94bc7de9ec79744d1b0c729bb94328
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-07-20 14:19:34 +0100

    implemented singly and doubly linked lists

---
+++ Listicle/Interfaces/IDoublyLinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listicle.Interfaces
+{
+    public interface IDoublyLinkedList<T>
+    {
+        T Node { get; set; }
+
+        IDoublyLinkedList<T> Next { get; set; }
+        IDoublyLinkedList<T> Previous { get; set; }
+
+        void Add(T item);
+
+        void Delete(IDoublyLinkedList<T> Node);
+
+        IDoublyLinkedList<T> Find(T item);
+
+        T[] Values();
+    }
+}
---
+++ Listicle/Interfaces/ISinglyLinkedList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listicle.Interfaces
+{
+    public interface ISinglyLinkedList<T>
+    {
+        T Node { get; set; }
+
+        ISinglyLinkedList<T> Next { get; set; }
+
+        void Add(T item);
+
+        void Delete(ISinglyLinkedList<T> Node);
+
+        ISinglyLinkedList<T> Find(T item);
+
+        T[] Values();
+    }
+}
