6eda5cfdf1943b5f2c36be99d5fccb25f47a10e5
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-07-20 11:08:34 +0100

    Initial Commit

---
+++ Listicle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Listicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
