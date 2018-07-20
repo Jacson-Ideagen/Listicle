2f6d5337c0f882869d066f34876cbc457acd2338
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-07-20 14:21:18 +0100

    added initial unit tests for singly linked list

--- Listicle/Program.cs
+++ Listicle/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Listicle.Tests;
+using System;
 
 namespace Listicle
 {
     class Program
     {
+        private bool running = true;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prog = new Program();
+            prog.Run();
+        }
+        public void Run()
+        {
+            var slt = new SinglyLinkedListTests();
+            while (running)
+            {
+                Console.WriteLine(slt.AddTest());
+                var comm = TakeCommand();
+            }
+        }
+        public string TakeCommand()
+        {
+            var comm = Console.ReadLine();
+            switch (comm)
+            {
+                case "x":
+                    running = false;
+                    break;
+                default:
+                    return comm;
+            }
+            return comm;
         }
     }
 }
---
+++ Listicle/Tests/SinglyLinkedListTests.cs
@@ -0,0 +1,41 @@
+﻿using Listicle.Lists;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listicle.Tests
+{
+    class SinglyLinkedListTests
+    {
+        public bool AddTest()
+        {
+            var result = true;
+
+            var list = new SinglyLinkedList<string>();
+
+            list.Add("jim");
+            if(list[0].Node != "jim")
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public bool FindTest()
+        {
+            var result = true;
+
+            var list = new SinglyLinkedList<string>();
+
+            list.Add("jim");
+            list.Add("dave");
+
+            if (list.Find("dave") == null)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
