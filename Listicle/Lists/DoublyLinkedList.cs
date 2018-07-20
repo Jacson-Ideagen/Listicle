050ed89a8e9ff4cdaf4b897db0e6e3025c5752d8
Jacson Curtis <jacson.curtis@ideagenplc.com>
2018-07-20 14:20:31 +0100

    implemented singly and doubly linked lists

---
+++ Listicle/Lists/DoublyLinkedList.cs
@@ -0,0 +1,60 @@
+﻿using Listicle.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listicle.Lists
+{
+    public class DoublyLinkedList<T> : IDoublyLinkedList<T>
+    {
+        public T Node { get; set; }
+        public IDoublyLinkedList<T> Next { get; set; }
+        public IDoublyLinkedList<T> Previous { get; set; }
+
+        public void Add(T item)
+        {
+            if (Next is null)
+            {
+                Next.Add(item);
+            }
+            else
+            {
+                Next = new DoublyLinkedList<T>
+                {
+                    Node = item,
+                    Previous = this
+                };
+            }
+        }
+
+        public void Delete(IDoublyLinkedList<T> Node)
+        {
+            if (Next == Node)
+            {
+                Next = Next.Next;
+            }
+        }
+
+        public IDoublyLinkedList<T> Find(T item)
+        {
+            if (Next.Node.Equals(item))
+            {
+                return Next;
+            }
+            else
+            {
+                return Next.Find(item);
+            }
+        }
+
+        public T[] Values()
+        {
+            var vals = new List<T>() { Node };
+            if (!(Next is null))
+            {
+                vals.AddRange(new List<T>(Next.Values()));
+            }
+            return vals.ToArray();
+        }
+    }
+}
---
+++ Listicle/Lists/SinglyLinkedList.cs
@@ -0,0 +1,71 @@
+﻿using Listicle.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Listicle.Lists
+{
+    public class SinglyLinkedList<T> : ISinglyLinkedList<T>
+    {
+        public T Node { get; set; }
+
+        public ISinglyLinkedList<T> Next { get; set; }
+
+        public ISinglyLinkedList<T> this[int index]
+        {
+            get
+            {
+                var result = this;
+                while(index > 0)
+                {
+                    result = (SinglyLinkedList<T>)result.Next;
+                    index--;
+                }
+                return result;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Node == null)
+            {
+                Node = item;
+                Next = new SinglyLinkedList<T>();
+            }
+            else
+            {
+                Next.Add(item);
+            }
+        }
+
+        public void Delete(ISinglyLinkedList<T> Node)
+        {
+            if (Next == Node)
+            {
+                Next = Next.Next;
+            }
+        }
+
+        public ISinglyLinkedList<T> Find(T item)
+        {
+            if (Next.Node.Equals(item))
+            {
+                return Next;
+            }
+            else
+            {
+                return Next.Find(item);
+            }
+        }
+
+        public T[] Values()
+        {
+            var vals = new List<T>() { Node };
+            if (!(Next is null))
+            {
+                vals.AddRange(new List<T>(Next.Values()));
+            }
+            return vals.ToArray();
+        }
+    }
+}
